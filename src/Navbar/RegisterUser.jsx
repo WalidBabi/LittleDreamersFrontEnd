117f36890c8f4e05623c8c500770fe4f9b4c6651
Bashar Usif <bashar.usif@smartsoft.sy>
2024-02-29 19:56:47 +0300

    add agree of policy to register

--- src/Navbar/RegisterUser.jsx
+++ src/Navbar/RegisterUser.jsx
@@ -20,6 +20,9 @@ function RegisterUser() {
         };
   });
 
+  // New state for the checkbox
+  const [isAgreed, setIsAgreed] = useState(false);
+
   const [errors, setErrors] = useState({
     firstName: false,
     lastName: false,
@@ -38,6 +41,11 @@ function RegisterUser() {
     setErrors({ ...errors, [name]: false });
   };
 
+  // Handle checkbox state change
+  const handleCheckboxChange = () => {
+    setIsAgreed(!isAgreed);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     let hasErrors = false;
@@ -48,6 +56,11 @@ function RegisterUser() {
         newErrors[key] = true;
         hasErrors = true;
       }
+    }
+
+    if (!isAgreed) {
+      // Check if the checkbox is not checked
+      hasErrors = true;
     }
 
     if (formData.password !== formData.confirmPassword) {
@@ -227,9 +240,28 @@ function RegisterUser() {
                 </p>
               )}
             </div>
+            <div className="mb-4">
+              <label className="flex items-center">
+                <input
+                  type="checkbox"
+                  checked={isAgreed}
+                  onChange={handleCheckboxChange}
+                  className="form-checkbox h-5 w-5 text-blue-500"
+                />
+                <span className="ml-2 text-gray-700">
+                  I agree to the{" "}
+                  <Link to="/User-product-policy" className="text-blue-500">
+                    User Policy and Terms
+                  </Link>
+                </span>
+              </label>
+            </div>
             <button
+              disabled={!isAgreed} // Disable the button if not agreed
               onClick={handleSubmit}
-              className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition duration-300"
+              className={`w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-md ${
+                !isAgreed ? "opacity-50 cursor-not-allowed" : "" // Reduce opacity and disable cursor if not agreed
+              } hover:bg-blue-600 transition duration-300`}
             >
               Register
             </button>
