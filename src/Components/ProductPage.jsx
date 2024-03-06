3821410bf0e5ee5435b2dc6a8a687476911137df
Bashar Usif <bashar.usif@smartsoft.sy>
2024-03-06 20:46:05 +0300

    add warning popup & update imports

--- src/Components/ProductPage.jsx
+++ src/Components/ProductPage.jsx
@@ -1,17 +1,29 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useCart } from "./ShoppingCart/CartContext";
+import { useCart } from "../Context/CartContext";
 import { useParams } from "react-router-dom";
 import LoadingAnimation from "../Layout/Loading/LoadingAnimation";
+import Warning from "../Layout/Modal/Warning/Warning";
 
 const ProductPage = () => {
   const { id } = useParams();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showWarningModal, setShowWarningModal] = useState(false);
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+  const { addToCart, updateCartItemQuantity, itemAddedToCart, cartItems } =
+    useCart();
+
+  useEffect(() => {
+    // Check if the user is logged in
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(!!token);
+  }, []);
 
   const [product, setProduct] = useState([]);
   const [description, setDescription] = useState([]);
   const [quantity, setQuantity] = useState(1);
   const [rating, setRating] = useState(null);
-  const [loading, setLoading] = useState(true); // Add loading state
+  const [loading, setLoading] = useState(true);
 
   const infoItems = [
     { label: "Category", value: description.category },
@@ -23,15 +35,11 @@ const ProductPage = () => {
   ];
 
   useEffect(() => {
-    // Check if id is truthy before making the API request
     if (id) {
-      setLoading(true); // Set loading to true when fetching datas
-      // Fetch product details from the backend
+      setLoading(true);
       axios
         .get(`http://localhost:8000/api/products/${id}`)
         .then((response) => {
-          // just to check Product data
-          //console.log("Product data:", response.data);
           setProduct(response.data.product);
           setDescription(response.data.description);
         })
@@ -39,11 +47,10 @@ const ProductPage = () => {
           console.error("Error fetching product details:", error);
         })
         .finally(() => {
-          setLoading(false); // Set loading to false regardless of success or failure
+          setLoading(false);
         });
     }
 
-    // Fetch user's rating from local storage
     const storedRating = localStorage.getItem(`${id}-rating`);
     if (storedRating !== null) {
       setRating(parseInt(storedRating, 10));
@@ -51,24 +58,24 @@ const ProductPage = () => {
   }, [id]);
 
   const handleRatingChange = (newRating) => {
+    if (!isLoggedIn) {
+      alert("Please login to rate this product");
+      return;
+    }
     setRating(newRating);
 
-    // Save the rating to local storage
     localStorage.setItem(`${id}-rating`, newRating);
 
-    // Get the token from local storage
     const token = localStorage.getItem("token");
 
-    // Check if token is available
     if (!token) {
       console.error("Token not found in localStorage");
       return;
     }
 
-    // Send the rating to the backend using Axios
     axios
       .post(
-        `http://localhost:8000/api/Review/${id}/rating=${newRating}`, // Fix: use id instead of productId
+        `http://localhost:8000/api/Review/${id}/rating=${newRating}`,
         {
           productId: id,
           rating: newRating,
@@ -87,8 +94,6 @@ const ProductPage = () => {
       });
   };
 
-  const { addToCart, updateCartItemQuantity } = useCart();
-
   const handleIncreaseQuantity = () => {
     setQuantity(quantity + 1);
   };
@@ -100,25 +105,36 @@ const ProductPage = () => {
   };
 
   const handleAddToBag = () => {
+    if (!isLoggedIn) {
+      setShowWarningModal(true);
+      return;
+    }
+
     const existingCartItem = addToCart({
       id: product.id,
       name: product.name,
       price: product.price,
       quantity: quantity,
     });
+    setShowSuccessAlert(true);
+    // Clear the flag after a certain time (e.g., 5 seconds)
+    // setItemAddedToCart(true);
+    setTimeout(() => {
+      setShowSuccessAlert(false);
+      // setItemAddedToCart(false);
+      // Reload the page after the success message is displayed
+      window.location.reload();
+    }, 1000);
 
-    // Save the cart items to local storage
     const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
     const existingItemIndex = cartItems.findIndex(
       (item) => item.id === product.id
     );
 
     if (existingItemIndex !== -1) {
-      // If the product is already in the cart, update the quantity and rating
       cartItems[existingItemIndex].quantity += quantity;
       cartItems[existingItemIndex].rating = rating;
     } else {
-      // Otherwise, add a new item to the cart
       cartItems.push({
         id: product.id,
         name: product.name,
@@ -132,99 +148,122 @@ const ProductPage = () => {
 
     console.log(`Added ${quantity} item(s) to the bag with rating ${rating}`);
 
-    // If the product is already in the cart, update the quantity in the UI
     if (existingCartItem) {
       updateCartItemQuantity(product.id, existingCartItem.quantity + quantity);
     }
   };
 
+  const closeModal = () => {
+    setShowWarningModal(false);
+  };
+
   return (
-    <div className="container mx-auto mt-4 mb-8 flex">
-      {loading && <LoadingAnimation />}{" "}
-      {/* Render loading animation when loading is true */}
-      {!loading && (
-        <React.Fragment>
-          <div className="w-1/2">
-            <img
-              src={product.image}
-              alt="Product"
-              className="w-full rounded-lg shadow-lg"
-              style={{ maxWidth: "100%", height: "80%" }}
-            />
-          </div>
-          <div className="w-1/2 pl-8">
-            <h2 className="text-3xl font-semibold mb-4">{product.name}</h2>
-            <p className="text-2xl mb-4">${product.price}</p>
-            {product.dynamicRating !== null && (
-              <p className="text-lg mb-4">Rating: {product.dynamicRating}</p>
-            )}
-            <div className="flex items-center mb-4">
-              <p className="text-lg mb-2"></p>
-              {[1, 2, 3, 4, 5].map((star) => (
-                <button
-                  key={star}
-                  onClick={() => handleRatingChange(star)}
-                  className={`text-xl mx-1 ${
-                    rating >= star ? "text-yellow-500" : "text-gray-300"
-                  }`}
-                >
-                  &#9733;
-                </button>
-              ))}
+    <>
+      {/* Display a success alert */}
+      {showSuccessAlert && (
+        <div className="bg-green-500 text-white px-4 py-4 rounded-md">
+          Item added to the bag successfully!
+        </div>
+      )}
+      <div className="container mx-auto mt-4 mb-8 flex">
+        {loading && <LoadingAnimation />}{" "}
+        {/* Render loading animation when loading is true */}
+        {!loading && (
+          <React.Fragment>
+            <div className="w-1/2">
+              <img
+                src={product.image}
+                alt="Product"
+                className="w-full rounded-lg shadow-lg"
+                style={{ maxWidth: "100%", height: "80%" }}
+              />
             </div>
-            <div className="flex items-center mb-4">
-              <button
-                onClick={handleDecreaseQuantity}
-                className="bg-gray-300 text-gray-700 px-4 py-2 rounded-l"
-              >
-                -
-              </button>
-              <span className="px-4 text-2xl">{quantity}</span>
-              <button
-                onClick={handleIncreaseQuantity}
-                className="bg-gray-300 text-gray-700 px-4 py-2 rounded-r"
-              >
-                +
-              </button>
-            </div>
-            <button
-              onClick={handleAddToBag}
-              className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 transition duration-300"
-            >
-              Add to Bag
-            </button>
-            <div className="mt-8">
-              {/* Dynamic Information Section */}
-              <div className="mb-8 bg-gray-100 p-6 rounded-md shadow-lg">
-                <h3 className="text-lg font-extrabold mb-4 text-black">
-                  Product Details
-                </h3>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
-                  {infoItems.map((item) => (
-                    <div
-                      key={item.label}
-                      className="flex flex-col items-center p-4 bg-white rounded-md shadow-md
-                  hover:bg-gray-200 transition duration-300"
+            <div className="w-1/2 pl-8">
+              <h2 className="text-3xl font-semibold mb-4">{product.name}</h2>
+              <p className="text-2xl mb-4">${product.price}</p>
+              {product.dynamicRating !== null && isLoggedIn && (
+                <p className="text-lg mb-4">Rating: {product.dynamicRating}</p>
+              )}
+              {isLoggedIn && (
+                <div className="flex items-center mb-4">
+                  <p className="text-lg mb-2"></p>
+                  {[1, 2, 3, 4, 5].map((star) => (
+                    <button
+                      key={star}
+                      onClick={() => handleRatingChange(star)}
+                      className={`text-xl mx-1 ${
+                        rating >= star ? "text-yellow-500" : "text-gray-300"
+                      }`}
                     >
-                      <span className="text-md font-semibold mb-2 text-gray-600">
-                        {item.label}
-                      </span>
-                      <p className="text-sm text-indigo-700">{item.value}</p>
-                    </div>
+                      &#9733;
+                    </button>
                   ))}
                 </div>
+              )}
+              <div className="flex items-center mb-4">
+                <button
+                  onClick={handleDecreaseQuantity}
+                  className="bg-gray-300 text-gray-700 px-4 py-2 rounded-l"
+                >
+                  -
+                </button>
+                <span className="px-4 text-2xl">{quantity}</span>
+                <button
+                  onClick={handleIncreaseQuantity}
+                  className="bg-gray-300 text-gray-700 px-4 py-2 rounded-r"
+                >
+                  +
+                </button>
               </div>
+              <button
+                onClick={handleAddToBag}
+                className={`bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600 ${
+                  !isLoggedIn ? "cursor-not-allowed" : "hover:bg-blue-600"
+                } transition duration-300`}
+              >
+                Add to Bag
+              </button>
+              {!isLoggedIn && showWarningModal && (
+                <Warning
+                  message="Please login to add this product to your bag."
+                  onClose={closeModal}
+                />
+              )}
+              <div className="mt-8">
+                {/* Dynamic Information Section */}
+                <div className="mb-8 bg-gray-100 p-6 rounded-md shadow-lg">
+                  <h3 className="text-lg font-extrabold mb-4 text-black">
+                    Product Details
+                  </h3>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+                    {infoItems.map((item) => (
+                      <div
+                        key={item.label}
+                        className="flex flex-col items-center p-4 bg-white rounded-md shadow-md
+                  hover:bg-gray-200 transition duration-300"
+                      >
+                        <span className="text-md font-semibold mb-2 text-gray-600">
+                          {item.label}
+                        </span>
+                        <p className="text-sm text-indigo-700">{item.value}</p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
 
-              <h5 className="text-xl mb-4">{product.company}</h5>
-              <h3 className="text-2xl font-semibold mb-4">
-                Product Description
-              </h3>
-              <p className="text-lg text-gray-700">{description.description}</p>
+                <h5 className="text-xl mb-4">{product.company}</h5>
+                <h3 className="text-2xl font-semibold mb-4">
+                  Product Description
+                </h3>
+                <p className="text-lg text-gray-700">
+                  {description.description}
+                </p>
+              </div>
             </div>
-          </div>
-        </React.Fragment>
-      )}
-    </div>
+          </React.Fragment>
+        )}
+      </div>
+    </>
   );
 };
 
